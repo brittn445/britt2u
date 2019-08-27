b1b38dad9d7cb74f61ec348181ce4d43223f309a
t1809284_s <t1809284_s@NEITD-211218>
2019-08-26 19:06:49 -0500

    classwork2

--- classwork/ConsoleApp1/ConsoleApp1/Program.cs
+++ classwork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,16 +1,35 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace HelloWorld
 {
     class Program
     { 
         static void Main(string[] args)
         {
+            string name = "";
+
+            //string if = ";
+
+            //Definitely assigned
+           //name = "bob";
+            string name2;
+            name2 = Console.Readlne ();
+            Console.WriteLine (name2);
+
+
             Console.WriteLine("Hello World!");
             //TODO: Fix this
             //Another block
 
             //yet another block
+
+            int hours = 8;
+            double payRate = 15.25;
+
+
+            double totalPay = payRate * hours;
+
+            string fullName = "fred" + " "
         }
     }
 }
