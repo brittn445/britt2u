c4b4f604f2749288e71e4e2ae19b94614d502282
t1809284_s <t1809284_s@NEITD-211218>
2019-08-28 19:12:07 -0500

    class work 3

--- classwork/ConsoleApp1/ConsoleApp1/Program.cs
+++ classwork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,9 +3,122 @@
 namespace HelloWorld
 {
     class Program
-    { 
-        static void Main(string[] args)
+    {
+        static void Main ( /*string[] args*/ )
         {
+            //Movie data
+            string title;
+            int runLength;
+            int releaseYear;
+            string description;
+            bool haveSeen;
+
+            while (true)
+            {
+
+                char input = DisplayMenu ();
+                if (input == 'A')
+                    AddMovie ();
+                else if (input == 'Q')
+                    break;
+            };
+        }
+
+        static void AddMovie ()
+        {
+            //Get title
+            Console.Write("Title: ");
+            string title = Console.ReadLine ();
+
+            //Get description
+            Console.Write ("Description: ");
+            string description = Console.ReadLine ();
+
+            //Get release year
+            int releaseYear = ReadInt32 ("Release Year: ");
+
+
+
+            //Get run length
+            int runlength = ReadInt32 ("Run Legth (in minutes: ");
+
+
+            //Get have seen
+            bool haveSeen = ReadBoolean ("Have Seen? ");
+            
+
+
+
+        }
+
+
+        static int ReadInt32 ( string message )
+        {
+            while (true)
+            {
+
+
+                Console.Write (message);
+
+                string input = Console.ReadLine ();
+
+                //int result = Int32.Parse (input);
+                //int result;
+                //if (Int32.TryParse (input, out result))
+                if (Int32.TryParse(input, out int result))
+
+                    return result;
+
+                Console.Write ("Not a number");
+
+
+            }
+        }
+        static bool ReadBoolean ( string message )
+        {
+            while (true)
+            {
+
+
+                Console.Write (message);
+
+                string input = Console.ReadLine ();
+
+                //int result = Int32.Parse (input);
+                bool result;
+                if (Boolean.TryParse (input, out result))
+                    return result;
+
+                Console.Write ("Not a boolean");
+            }
+        }
+
+
+
+
+                static char DisplayMenu ()
+        {
+            do
+            {
+
+                Console.WriteLine ("A)dd Movie");
+                Console.WriteLine ("Q)uit");
+
+                String input = Console.ReadLine ();
+                if (input == "A" || input == "a")
+                {
+                    return 'A';
+                } else if (input == "Q" || input == "q")
+                {
+                    return 'Q';
+                } else
+                {
+                    Console.WriteLine ("Invalid input");
+                }
+
+            } while (true);
+        }
+        static void dayOneStuff(){
             string name = "";
 
             //string if = ";
@@ -13,7 +126,7 @@ namespace HelloWorld
             //Definitely assigned
            //name = "bob";
             string name2;
-            name2 = Console.Readlne ();
+            name2 = Console.ReadLine ();
             Console.WriteLine (name2);
 
 
@@ -29,7 +142,7 @@ namespace HelloWorld
 
             double totalPay = payRate * hours;
 
-            string fullName = "fred" + " "
+            string fullName = "fred" ;
         }
     }
 }
